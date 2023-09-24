79755e34dba7f3db06e142f51533b248e1f224f7
--global <--global>
2023-09-23 19:15:17 -0500

    added FarmMap

--- src/app/Template code/Login.jsx
+++ src/app/Template code/Login.jsx
@@ -2,7 +2,7 @@ import GroupDashboard from "./GroupDashboard";
 
 export default function Login() {
   // Create user object/request from db
-  const user = { ID: "12234" };
+  const user = { ID: "12234" , Location: []};
   //After login is succesful:
   return (
     <div>
