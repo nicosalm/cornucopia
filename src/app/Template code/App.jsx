e4094feb446e60f05850ea0bb5fb5ee2370e555d
Nico <ns@nicosalm.dev>
2023-09-24 11:53:17 -0500

    seaborn graph adjustments in app/python/chart.py

--- src/app/Template code/App.jsx
+++ src/app/Template code/App.jsx
@@ -4,7 +4,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <Login />
+      <Login/>
     </div>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import Dashboard from "./dashboard/Dashboard"
+import Login from "./login/page"
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
